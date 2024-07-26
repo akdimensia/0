e8b59e60c268737b49f0089125949e07de11a20d
akdsolutions <Arjunexisted@gmail.com>
2024-07-26 21:26:17 +0530

    hththt

--- tools/script.js
+++ tools/script.js
@@ -1,4 +1,5 @@
 // Get references to the popup elements
+alert('hi visitors this app is still under development and checkout my github repo for source code //Arjun');
 const popup = document.getElementById('popup');
 const popupIframe = document.getElementById('popupIframe');
 const closePopup = document.getElementById('closePopup');
